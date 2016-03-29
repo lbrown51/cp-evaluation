7e622a372824bf70047314e344352f022f0fa3d7
lbrown51 <lbrown51@users.noreply.github.com>
2016-03-29 19:38:25 +0000

    trying out import

--- site.js
+++ site.js
@@ -2,4 +2,13 @@ console.log("Hello world");
 
 $(document).ready(function() {
     console.log("rejoice - jquery is enabled!");
+    $.getJSON( "https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/7cc04951d4b0421e664ebbc9e4aa6642e2c02dbb/weather.json", function( data ) {
+        
+        data.each(function(
+            $("#weather").add("<li>"+this["TimeEST"]+"</li>");
+        });
+        var items = [];
+ 
+    });
 });
+
