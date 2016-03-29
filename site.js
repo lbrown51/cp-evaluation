c11c24b8957637d867b4bc29a0941a43b547bb4d
lbrown51 <lbrown51@users.noreply.github.com>
2016-03-29 19:45:06 +0000

    Test

--- site.js
+++ site.js
@@ -1,11 +1,12 @@
 console.log("Hello world");
 
 $(document).ready(function() {
-    console.log("rejoice - jquery is enabled!");
+    console.log($("u"));
     $.getJSON( "https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/7cc04951d4b0421e664ebbc9e4aa6642e2c02dbb/weather.json", function( data ) {
-        console.log($("#weather"));
+        console.log($("ul"));
         data.each(function(){
-            $("#weather").append("<li>"+this+"</li>");
+            console.log(this)
+            $("ul").append("<li>"+this+"</li>");
         });
         var items = [];
  
