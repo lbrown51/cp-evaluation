bf905b21c14dc96ff6b18162e3fc434e1f2449c5
lbrown51 <lbrown51@users.noreply.github.com>
2016-03-29 19:47:11 +0000

    faefse

--- site.js
+++ site.js
@@ -1,11 +1,11 @@
 console.log("Hello world");
 
 $(document).ready(function() {
-    console.log($("u"));
+    console.log($("ul"));
     $.getJSON( "https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/7cc04951d4b0421e664ebbc9e4aa6642e2c02dbb/weather.json", function( data ) {
         console.log($("ul"));
-        data.each(function(){
-            console.log(this)
+        data.each(function(key){
+            console.log(th)
             $("ul").append("<li>"+this+"</li>");
         });
         var items = [];
