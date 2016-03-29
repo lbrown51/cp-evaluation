9ea7573d6344033ca6594a63ba8c54d7cad16b13
lbrown51 <lbrown51@users.noreply.github.com>
2016-03-29 19:49:20 +0000

    Commit last

--- site.js
+++ site.js
@@ -1,15 +1,15 @@
 console.log("Hello world");
 
 $(document).ready(function() {
-    console.log($("ul"));
     $.getJSON( "https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/7cc04951d4b0421e664ebbc9e4aa6642e2c02dbb/weather.json", function( data ) {
-        console.log($("ul"));
+        console.log(data.length);
         data.each(function(key){
-            console.log(th)
+            console.log(key);
             $("ul").append("<li>"+this+"</li>");
         });
         var items = [];
  
     });
+    c
 });
 
